795b145222566fe4865015b7b48f7ec046ab2fff
JarrenBack <jarren.back@gmail.com>
2016-02-19 21:00:12 -0600

    Populated Labyrinth Class with add, remove, and see

--- app/src/main/java/gaming/wolfback/nonirim/Labyrinth.java
+++ app/src/main/java/gaming/wolfback/nonirim/Labyrinth.java
@@ -1,9 +1,37 @@
 package gaming.wolfback.nonirim;
 
 
+import java.util.List;
+
 /**
  * Created by Jarren on 2/19/2016.
  */
 public class Labyrinth {
+    private List<Card> labList;
+    public Labyrinth()
+    {
+    }
+    public void addCard(Card newCard)
+    {
+        labList.add(newCard);
+    }
 
+    void removeCard(Card toRemoveCard)
+    {
+        labList.remove(toRemoveCard);
+    }
+
+    String seeLab()
+    {
+        //int i = 0;
+        String theLabString = "";
+
+        for (int i = 0; i < labList.size(); ++i)
+        {
+            theLabString.concat("Card #" + i + labList.get(i).getColor() +
+                    " " + labList.get(i).getType() + " ");
+        }
+        System.out.println(theLabString);
+        return theLabString;
+    }
 }
