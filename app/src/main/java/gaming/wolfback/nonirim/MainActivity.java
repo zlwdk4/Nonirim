33347eb47223ecab4d344656284561d2e43aa130
JarrenBack <jarren.back@gmail.com>
2016-02-24 21:17:24 -0600

    Graphical Lab

--- app/src/main/java/gaming/wolfback/nonirim/MainActivity.java
+++ app/src/main/java/gaming/wolfback/nonirim/MainActivity.java
@@ -2,10 +2,12 @@ package gaming.wolfback.nonirim;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
 import android.widget.ImageButton;
+import android.widget.ImageView;
 import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
@@ -17,6 +19,8 @@ public class MainActivity extends AppCompatActivity {
     public ImageButton c3;
     public ImageButton c4;
     public TextView discard;
+    public int resId;
+    public String theCardString;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -29,112 +33,176 @@ public class MainActivity extends AppCompatActivity {
     }
 
     public void setInitialCardsInHand(){
-        String theDoorString;
-        int resId;
+        //String theCardString;
+        //int resId;
 
         c0 = (ImageButton) findViewById(R.id.hand0);
-        theDoorString = theFacade.getCardFromHand(0);
-        if (theDoorString == "nightmarenightmare") {
-            theDoorString = "nightmare";
+        theCardString = theFacade.getCardFromHand(0);
+        if (theCardString.equals("nightmarenightmare")) {
+            theCardString = "nightmare";
         }
-        resId = getResources().getIdentifier(theDoorString, "drawable", getPackageName());
+        resId = getResources().getIdentifier(theCardString, "drawable", getPackageName());
         c0.setImageResource(resId);
 
         c1 = (ImageButton) findViewById(R.id.hand1);
-        theDoorString = theFacade.getCardFromHand(1);
-        if (theDoorString == "nightmarenightmare") {
-            theDoorString = "nightmare";
+        theCardString = theFacade.getCardFromHand(1);
+        if (theCardString.equals("nightmarenightmare")) {
+            theCardString = "nightmare";
         }
-        resId = getResources().getIdentifier(theDoorString, "drawable", getPackageName());
+        resId = getResources().getIdentifier(theCardString, "drawable", getPackageName());
         c1.setImageResource(resId);
 
         c2 = (ImageButton) findViewById(R.id.hand2);
-        theDoorString = theFacade.getCardFromHand(2);
-        if (theDoorString == "nightmarenightmare") {
-            theDoorString = "nightmare";
+        theCardString = theFacade.getCardFromHand(2);
+        if (theCardString.equals("nightmarenightmare")) {
+            theCardString = "nightmare";
         }
-        resId = getResources().getIdentifier(theDoorString, "drawable", getPackageName());
+        resId = getResources().getIdentifier(theCardString, "drawable", getPackageName());
         c2.setImageResource(resId);
 
         c3 = (ImageButton) findViewById(R.id.hand3);
-        theDoorString = theFacade.getCardFromHand(3);
-        if (theDoorString == "nightmarenightmare") {
-            theDoorString = "nightmare";
+        theCardString = theFacade.getCardFromHand(3);
+        if (theCardString.equals("nightmarenightmare")) {
+            theCardString = "nightmare";
         }
-        resId = getResources().getIdentifier(theDoorString, "drawable", getPackageName());
+        resId = getResources().getIdentifier(theCardString, "drawable", getPackageName());
         c3.setImageResource(resId);
 
         c4 = (ImageButton) findViewById(R.id.hand4);
-        theDoorString = theFacade.getCardFromHand(4);
-        if (theDoorString == "nightmarenightmare") {
-            theDoorString = "nightmare";
+        theCardString = theFacade.getCardFromHand(4);
+        if (theCardString.equals("nightmarenightmare")) {
+            theCardString = "nightmare";
         }
-        resId = getResources().getIdentifier(theDoorString, "drawable", getPackageName());
+        resId = getResources().getIdentifier(theCardString, "drawable", getPackageName());
         c4.setImageResource(resId);
 
     }
 
     public void setOnClickListenersForHand(){
+        //String theCardString;
+        //int resId;
         c0.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
+                theCardString = theFacade.getCardFromHand(0);
+                if (theCardString.equals("nightmarenightmare")) {
+                    theCardString = "nightmare";
+                }
+
+                resId = getResources().getIdentifier(theCardString, "drawable", getPackageName());
                 //Takes the index of the card in the hand, removes the card from hand, and adds the card into the labyrinth
                 theFacade.playCardIntoLab(0);
-                String theLabString = theFacade.seeLab();
-                TextView theLab = (TextView) findViewById(R.id.LabId);
-                //
-                theLab.setText(theLabString);
-                theFacade.drawFromDeckIntoHand();
-                //c0.setText(theFacade.getCardFromHand(0));
 
+                ImageView theLab = (ImageView) findViewById(R.id.LabId);
+                theLab.setImageResource(resId);
+
+                theFacade.drawFromDeckIntoHand();
+                theCardString = theFacade.getCardFromHand(0);
+                if (theCardString.equals("nightmarenightmare")) {
+                    theCardString = "nightmare";
+                }
+                resId = getResources().getIdentifier(theCardString, "drawable", getPackageName());
+                c0.setImageResource(resId);
             }
         });
 
         c1.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
+                theCardString = theFacade.getCardFromHand(1);
+                if (theCardString.equals("nightmarenightmare")) {
+                    theCardString = "nightmare";
+                }
+
+                resId = getResources().getIdentifier(theCardString, "drawable", getPackageName());
+                //Takes the index of the card in the hand, removes the card from hand, and adds the card into the labyrinth
                 theFacade.playCardIntoLab(1);
-                String theLabString = theFacade.seeLab();
-                TextView theLab = (TextView) findViewById(R.id.LabId);
-                theLab.setText(theLabString);
+
+                ImageView theLab = (ImageView) findViewById(R.id.LabId);
+                theLab.setImageResource(resId);
+
                 theFacade.drawFromDeckIntoHand();
-                //c1.setText(theFacade.getCardFromHand(1));
+                theCardString = theFacade.getCardFromHand(1);
+                if (theCardString.equals("nightmarenightmare")) {
+                    theCardString = "nightmare";
+                }
+                resId = getResources().getIdentifier(theCardString, "drawable", getPackageName());
+                c1.setImageResource(resId);
             }
         });
 
         c2.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
+                theCardString = theFacade.getCardFromHand(2);
+                if (theCardString.equals("nightmarenightmare")) {
+                    theCardString = "nightmare";
+                }
+
+                resId = getResources().getIdentifier(theCardString, "drawable", getPackageName());
+                //Takes the index of the card in the hand, removes the card from hand, and adds the card into the labyrinth
                 theFacade.playCardIntoLab(2);
-                String theLabString = theFacade.seeLab();
-                TextView theLab = (TextView) findViewById(R.id.LabId);
-                theLab.setText(theLabString);
+
+                ImageView theLab = (ImageView) findViewById(R.id.LabId);
+                theLab.setImageResource(resId);
+
                 theFacade.drawFromDeckIntoHand();
-                //c2.setText(theFacade.getCardFromHand(2));
+                theCardString = theFacade.getCardFromHand(2);
+                if (theCardString.equals("nightmarenightmare")) {
+                    theCardString = "nightmare";
+                }
+                resId = getResources().getIdentifier(theCardString, "drawable", getPackageName());
+                c2.setImageResource(resId);
             }
         });
 
         c3.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
+                theCardString = theFacade.getCardFromHand(3);
+                if (theCardString.equals("nightmarenightmare")) {
+                    theCardString = "nightmare";
+                }
+
+                resId = getResources().getIdentifier(theCardString, "drawable", getPackageName());
+                //Takes the index of the card in the hand, removes the card from hand, and adds the card into the labyrinth
                 theFacade.playCardIntoLab(3);
-                String theLabString = theFacade.seeLab();
-                TextView theLab = (TextView) findViewById(R.id.LabId);
-                theLab.setText(theLabString);
+
+                ImageView theLab = (ImageView) findViewById(R.id.LabId);
+                theLab.setImageResource(resId);
+
                 theFacade.drawFromDeckIntoHand();
-                //c3.setText(theFacade.getCardFromHand(3));
+                theCardString = theFacade.getCardFromHand(3);
+                if (theCardString.equals("nightmarenightmare")) {
+                    theCardString = "nightmare";
+                }
+                resId = getResources().getIdentifier(theCardString, "drawable", getPackageName());
+                c3.setImageResource(resId);
             }
         });
 
         c4.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
+                theCardString = theFacade.getCardFromHand(4);
+                if (theCardString.equals("nightmarenightmare")) {
+                    theCardString = "nightmare";
+                }
+
+                resId = getResources().getIdentifier(theCardString, "drawable", getPackageName());
+                //Takes the index of the card in the hand, removes the card from hand, and adds the card into the labyrinth
                 theFacade.playCardIntoLab(4);
-                String theLabString = theFacade.seeLab();
-                TextView theLab = (TextView) findViewById(R.id.LabId);
-                theLab.setText(theLabString);
+
+                ImageView theLab = (ImageView) findViewById(R.id.LabId);
+                theLab.setImageResource(resId);
+
                 theFacade.drawFromDeckIntoHand();
-                //c4.setText(theFacade.getCardFromHand(4));
+                theCardString = theFacade.getCardFromHand(4);
+                if (theCardString.equals("nightmarenightmare")) {
+                    theCardString = "nightmare";
+                }
+                resId = getResources().getIdentifier(theCardString, "drawable", getPackageName());
+                c4.setImageResource(resId);
             }
         });
     }
