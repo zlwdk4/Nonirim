48f49c6d910ada896e277b1601130b5d82be8091
Zachary Wolf <zlwdk4@mail.umkc.edu>
2016-04-26 00:39:53 -0500

    Prophecy With Order selection mechanics
    code to be cleaned

--- app/src/main/java/gaming/wolfback/nonirim/View/ProphecyScreen.java
+++ app/src/main/java/gaming/wolfback/nonirim/View/ProphecyScreen.java
@@ -1,17 +1,31 @@
 package gaming.wolfback.nonirim.View;
 
 import android.app.Activity;
+import android.content.ClipData;
+import android.content.DialogInterface;
 import android.content.Intent;
-import android.support.v7.app.AppCompatActivity;
+import android.graphics.Canvas;
+import android.graphics.Color;
+import android.graphics.Point;
+import android.graphics.drawable.ColorDrawable;
 import android.os.Bundle;
+import android.util.Log;
+import android.view.DragEvent;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.MotionEvent;
 import android.view.View;
 import android.widget.ImageView;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import org.w3c.dom.Text;
 
 import gaming.wolfback.nonirim.R;
 
 public class ProphecyScreen extends Activity {
+
+
 
     public String prophSelectionString = "";
 
@@ -36,6 +50,216 @@ public class ProphecyScreen extends Activity {
         setContentView(R.layout.activity_prophecy_screen);
 
 
+
+        class DragShadow extends View.DragShadowBuilder {
+
+            ColorDrawable greyBox;
+
+
+            public DragShadow(View view) {
+                super(view);
+                greyBox = new ColorDrawable(Color.BLACK);
+
+            }
+
+            @Override
+            public void onDrawShadow(Canvas canvas) {
+                greyBox.draw(canvas);
+            }
+
+            @Override
+            public void onProvideShadowMetrics(Point shadowSize, Point shadowTouchPoint) {
+                View v = getView();
+
+                int height = (int) v.getHeight() / 2;
+                int width = (int) v.getWidth() / 2;
+
+                greyBox.setBounds(0, 0, width, height);
+
+
+                shadowSize.set(width, height);
+
+                shadowTouchPoint.set((int) width / 2, (int) height / 2);
+            }
+        }
+
+
+        //DRAG AND DROP LOGIC
+
+
+
+
+
+
+
+        View.OnDragListener dropListener = new View.OnDragListener() {
+            @Override
+            public boolean onDrag(View v, DragEvent event) {
+                int dragEvent = event.getAction();
+                switch (dragEvent){
+                    case DragEvent.ACTION_DROP:
+                        View dragged = (View) event.getLocalState();
+                        ClipData theData = event.getClipData();
+                        int theInd;
+
+                        String theSelection = theData.getItemAt(0).coerceToText(getApplicationContext()).toString();
+                        
+
+                        if(v.getId() == R.id.proph_select_display_1){
+                            TextView tv = (TextView) findViewById(R.id.proph_display_1);
+                            tv.setText(theSelection);
+
+                        }
+
+                        else if(v.getId() == R.id.proph_select_display_2){
+                            TextView tv = (TextView) findViewById(R.id.proph_display_2);
+                            tv.setText(theSelection);
+
+                        }
+
+                        else if(v.getId() == R.id.proph_select_display_3){
+                            TextView tv = (TextView) findViewById(R.id.proph_display_3);
+                            tv.setText(theSelection);
+                        }
+
+                        else if(v.getId() == R.id.proph_select_display_4){
+                            TextView tv = (TextView) findViewById(R.id.proph_display_4);
+                            tv.setText(theSelection);
+                        }
+
+                        else if(v.getId() == R.id.proph_select_display_5){
+                            TextView tv = (TextView) findViewById(R.id.proph_display_5);
+                            tv.setText(theSelection);
+                        }
+
+                }
+                return true;
+            }
+        };
+
+
+
+        ////////////////////////////////////////////////////////////////////////////////
+        ////////////////////////////////////////////////////////////////////////////////
+
+        View.OnTouchListener touchListener1 = new View.OnTouchListener(){
+
+            @Override
+            public boolean onTouch(View v, MotionEvent event) {
+
+                DragShadow dragShadow = new DragShadow(v);
+
+                //used to pass the metadata through (card id being used)
+                ClipData data = ClipData.newPlainText("id", "1");
+
+                v.startDrag(data, dragShadow, v, 0);
+
+                return false;
+            }
+        };
+
+        View.OnTouchListener touchListener2 = new View.OnTouchListener(){
+
+            @Override
+            public boolean onTouch(View v, MotionEvent event) {
+
+                DragShadow dragShadow = new DragShadow(v);
+
+                //used to pass the metadata through (card id being used)
+                ClipData data = ClipData.newPlainText("id", "2");
+
+                v.startDrag(data, dragShadow, v, 0);
+
+                return false;
+            }
+        };
+
+        View.OnTouchListener touchListener3 = new View.OnTouchListener(){
+
+            @Override
+            public boolean onTouch(View v, MotionEvent event) {
+
+                DragShadow dragShadow = new DragShadow(v);
+
+                //used to pass the metadata through (card id being used)
+                ClipData data = ClipData.newPlainText("id", "3");
+
+                v.startDrag(data, dragShadow, v, 0);
+
+                return false;
+            }
+        };
+
+        View.OnTouchListener touchListener4 = new View.OnTouchListener(){
+
+            @Override
+            public boolean onTouch(View v, MotionEvent event) {
+
+                DragShadow dragShadow = new DragShadow(v);
+
+                //used to pass the metadata through (card id being used)
+                ClipData data = ClipData.newPlainText("id", "4");
+
+                v.startDrag(data, dragShadow, v, 0);
+
+                return false;
+            }
+        };
+
+
+
+
+        View.OnTouchListener touchListenerX = new View.OnTouchListener(){
+
+            @Override
+            public boolean onTouch(View v, MotionEvent event) {
+
+                DragShadow dragShadow = new DragShadow(v);
+
+                //used to pass the metadata through (card id being used)
+                ClipData data = ClipData.newPlainText("id", "X");
+
+                v.startDrag(data, dragShadow, v, 0);
+
+                return false;
+            }
+        };
+
+        ////////////////////////////////////////////////////////////////////////////////////
+        /////////////////////////////////////////////////////////////////////////////////////
+
+        ///SETTING TARGETS
+        ImageView prophTarget1 = (ImageView) findViewById(R.id.proph_select_display_1);
+        prophTarget1.setOnDragListener(dropListener);
+
+        ImageView prophTarget2 = (ImageView) findViewById(R.id.proph_select_display_2);
+        prophTarget2.setOnDragListener(dropListener);
+
+        ImageView prophTarget3 = (ImageView) findViewById(R.id.proph_select_display_3);
+        prophTarget3.setOnDragListener(dropListener);
+
+        ImageView prophTarget4 = (ImageView) findViewById(R.id.proph_select_display_4);
+        prophTarget4.setOnDragListener(dropListener);
+
+        ImageView prophTarget5 = (ImageView) findViewById(R.id.proph_select_display_5);
+        prophTarget5.setOnDragListener(dropListener);
+
+        TextView prophSelectOption1 = (TextView) findViewById(R.id.proph_option_1);
+        prophSelectOption1.setOnTouchListener(touchListener1);
+
+        TextView prophSelectOption2 = (TextView) findViewById(R.id.proph_option_2);
+        prophSelectOption2.setOnTouchListener(touchListener2);
+
+        TextView prophSelectOption3 = (TextView) findViewById(R.id.proph_option_3);
+        prophSelectOption3.setOnTouchListener(touchListener3);
+
+        TextView prophSelectOption4 = (TextView) findViewById(R.id.proph_option_4);
+        prophSelectOption4.setOnTouchListener(touchListener4);
+
+        TextView prophSelectOptionX = (TextView) findViewById(R.id.proph_option_x);
+        prophSelectOptionX.setOnTouchListener(touchListenerX);
+
+
         Intent activityThatCalled = getIntent();
 
         String [] prophCards = activityThatCalled.getExtras().getStringArray("prophArray");
@@ -54,6 +278,39 @@ public class ProphecyScreen extends Activity {
 
     }
 
+
+
+
+    public void prophFinishAction(View view){
+
+        String[] theOrder = new String[5];
+
+        getTheOrderFromViews(theOrder);
+
+        String retS = "";
+
+        for(int i = 0; i < 5; i++){
+            retS += theOrder[i];
+        }
+
+        Toast cbToast = Toast.makeText(getApplicationContext(), retS, Toast.LENGTH_LONG);
+        cbToast.show();
+
+
+
+        backToMainFromProph(view);
+    }
+
+    private String[] getTheOrderFromViews(String[] theOrder) {
+        TextView[] theViews = getProphDisplayViews();
+
+        for(int i = 0; i < 5; i++){
+            theOrder[i] = theViews[i].getText().toString();
+        }
+
+        return theOrder;
+    }
+
     public void backToMainFromProph(View view) {
 
         Intent prophRetIntent = new Intent();
@@ -62,6 +319,18 @@ public class ProphecyScreen extends Activity {
 
         finish();
     }
+
+    private TextView[] getProphDisplayViews(){
+        TextView[] theViews = new TextView[5];
+        theViews[0] = (TextView) findViewById(R.id.proph_display_1);
+        theViews[1] = (TextView) findViewById(R.id.proph_display_2);
+        theViews[2] = (TextView) findViewById(R.id.proph_display_3);
+        theViews[3] = (TextView) findViewById(R.id.proph_display_4);
+        theViews[4] = (TextView) findViewById(R.id.proph_display_5);
+
+        return  theViews;
+    }
+
 
 
     @Override
